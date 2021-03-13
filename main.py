7be88920ce0e99e3d6c5c13715dc4e22b6d79b16
Maxouille <xd.maxouille@gmail.com>
2021-03-13 21:28:17 +0100

    Update main.py
    raw to packed + packed to json

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import tkinter
 from tkinter import *
-from team_converter import export_to_packed
+from team_converter import export_to_packed, packed_to_json
+import os
 
-def writeFile():
+def pack_team():
     file = open('team.txt','w')
     file.close()
     file = open('team.txt','a+')
-    file.write(metinF.get() + '\n')
+    text = metinF.get("1.0",END)
+    file.write(text.strip() + '\n')
     file.close()
     file = open("team.txt", "r")
-    team_json = (file.read())
-    print(export_to_packed(team_json))
+    team_json = file.read()
+    print("|" + team_json.strip() + "|")
+    team = export_to_packed(team_json.strip())
+    print(team)
+    file.close()
+
+def unpack_team():
+    file = open('team.txt','w')
+    file.close()
+    file = open('team.txt','a+')
+    text = metinF.get("1.0",END)
+    file.write(text.strip() + '\n')
+    file.close()
+    file = open("team.txt", "r")
+    team_packed = file.read()
+    print("|" + team_packed.strip() + "|")
+    team = packed_to_json(team_packed.strip())
+    print(team)
     file.close()
 
 gui = Tk()
+gui.geometry("400x400")
 
 gui.title('le convetisseur de team')
 
-metinF = Entry(gui)
-metinF.grid(row=9, column=1)
+frame = Frame(gui)
+frame.pack()
 
-butonWrite = Button(gui)
-butonWrite.config(text = 'Conversion !', command = writeFile)
-butonWrite.grid(row=8, column=1)
+metinF = Text(gui, height=20, width=40)
+metinF.pack(side=BOTTOM)
+
+text_disp= Button(frame,
+                   text="Pack!",
+                   fg="red",
+                   command=pack_team
+                   )
+text_disp.pack(side=LEFT)
+
+exit_button = Button(frame,
+                   text="Unpack!",
+                   fg="green",
+                   command=unpack_team)
+exit_button.pack(side=RIGHT)
 
 gui.mainloop()
